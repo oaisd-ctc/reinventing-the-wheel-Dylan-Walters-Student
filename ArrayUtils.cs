93c37301f756d02d382b2f201d87e14c43a11cb9
Alexander-Wilhelm-Student <112420921+Alexander-Wilhelm-Student@users.noreply.github.com>
2023-01-23 18:33:36 +0000

    max, min, sum, product

--- ArrayUtils.cs
+++ ArrayUtils.cs
@@ -2,6 +2,8 @@
 {
     public class ArrayUtilities
     {
+
+        //average
         public double Average(int[] nums)
         {
             double x = 0;
@@ -58,6 +60,8 @@
             return x;
         }
 
+
+        //max
         public int Max(int[] nums)
         {
             if (nums.Length > 0)
@@ -80,7 +84,284 @@
                 }
             }
 
-            else throw new Exception("Average() received empty array!");
+            else throw new Exception();
+        }
+
+        public float Max(float[] nums)
+        {
+            if (nums.Length > 0)
+            {
+                float x = nums[0];
+                if (nums.Length == 1)
+                {
+                    return x;
+                }
+                else
+                {
+                    for (int i = 1; i < nums.Length; i++)
+                    {
+                        if (x < nums[i])
+                        {
+                            x = nums[i];
+                        }
+                    }
+                    return x;
+                }
+            }
+
+            else throw new Exception();
+        }
+
+        public double Max(double[] nums)
+        {
+            if (nums.Length > 0)
+            {
+                double x = nums[0];
+                if (nums.Length == 1)
+                {
+                    return x;
+                }
+                else
+                {
+                    for (int i = 1; i < nums.Length; i++)
+                    {
+                        if (x < nums[i])
+                        {
+                            x = nums[i];
+                        }
+                    }
+                    return x;
+                }
+            }
+
+            else throw new Exception();
+        }
+
+        public decimal Max(decimal[] nums)
+        {
+            if (nums.Length > 0)
+            {
+                decimal x = nums[0];
+                if (nums.Length == 1)
+                {
+                    return x;
+                }
+                else
+                {
+                    for (int i = 1; i < nums.Length; i++)
+                    {
+                        if (x < nums[i])
+                        {
+                            x = nums[i];
+                        }
+                    }
+                    return x;
+                }
+            }
+
+            else throw new Exception();
+        }
+
+
+        //min
+        public int Min(int[] nums)
+        {
+            if (nums.Length > 0)
+            {
+                int x = nums[0];
+                if (nums.Length == 1)
+                {
+                    return x;
+                }
+                else
+                {
+                    for (int i = 1; i < nums.Length; i++)
+                    {
+                        if (x > nums[i])
+                        {
+                            x = nums[i];
+                        }
+                    }
+                    return x;
+                }
+            }
+
+            else throw new Exception();
+        }
+
+        public float Min(float[] nums)
+        {
+            if (nums.Length > 0)
+            {
+                float x = nums[0];
+                if (nums.Length == 1)
+                {
+                    return x;
+                }
+                else
+                {
+                    for (int i = 1; i < nums.Length; i++)
+                    {
+                        if (x > nums[i])
+                        {
+                            x = nums[i];
+                        }
+                    }
+                    return x;
+                }
+            }
+
+            else throw new Exception();
+        }
+
+        public double Min(double[] nums)
+        {
+            if (nums.Length > 0)
+            {
+                double x = nums[0];
+                if (nums.Length == 1)
+                {
+                    return x;
+                }
+                else
+                {
+                    for (int i = 1; i < nums.Length; i++)
+                    {
+                        if (x > nums[i])
+                        {
+                            x = nums[i];
+                        }
+                    }
+                    return x;
+                }
+            }
+
+            else throw new Exception();
+        }
+
+        public decimal Min(decimal[] nums)
+        {
+            if (nums.Length > 0)
+            {
+                decimal x = nums[0];
+                if (nums.Length == 1)
+                {
+                    return x;
+                }
+                else
+                {
+                    for (int i = 1; i < nums.Length; i++)
+                    {
+                        if (x > nums[i])
+                        {
+                            x = nums[i];
+                        }
+                    }
+                    return x;
+                }
+            }
+
+            else throw new Exception();
+        }
+
+
+        //sum
+        public int Sum(int[] nums) {
+            if (nums.Length > 0) {
+                int x = 0;
+                foreach (int i in nums) {
+                    x += i;
+                }
+                return x;
+            }
+
+            else throw new Exception();
+        }
+
+        public float Sum(float[] nums) {
+            if (nums.Length > 0) {
+                float x = 0;
+                foreach (float i in nums) {
+                    x += i;
+                }
+                return x;
+            }
+
+            else throw new Exception();
+        }
+
+        public double Sum(double[] nums) {
+            if (nums.Length > 0) {
+                double x = 0;
+                foreach (double i in nums) {
+                    x += i;
+                }
+                return x;
+            }
+
+            else throw new Exception();
+        }
+
+        public decimal Sum(decimal[] nums) {
+            if (nums.Length > 0) {
+                decimal x = 0;
+                foreach (decimal i in nums) {
+                    x += i;
+                }
+                return x;
+            }
+
+            else throw new Exception();
+        }
+
+
+        //product
+        public int Product(int[] nums) {
+            if (nums.Length > 0) {
+                int x = 1;
+                foreach (int i in nums) {
+                    x *= i;
+                }
+                return x;
+            }
+
+            else throw new Exception();
+        }
+
+        public float Product(float[] nums) {
+            if (nums.Length > 0) {
+                float x = 1;
+                foreach (float i in nums) {
+                    x *= i;
+                }
+                return x;
+            }
+
+            else throw new Exception();
+        }
+
+        public double Product(double[] nums) {
+            if (nums.Length > 0) {
+                double x = 1;
+                foreach (double i in nums) {
+                    x *= i;
+                }
+                return x;
+            }
+
+            else throw new Exception();
+        }
+
+        public decimal Product(decimal[] nums) {
+            if (nums.Length > 0) {
+                decimal x = 1;
+                foreach (decimal i in nums) {
+                    x *= i;
+                }
+                return x;
+            }
+
+            else throw new Exception();
         }
 
     }
