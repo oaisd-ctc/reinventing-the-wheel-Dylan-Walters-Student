810fa1b82fe5085656877340c1e6b896346f5a26
Dylan-Walters-Student <dywa55d@student.oaisd.org>
2023-01-24 14:44:33 -0500

    started string util

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(MathUtilities.Binary(1048576));
+        
     }
 }
--- StringUtils.cs
+++ StringUtils.cs
@@ -1,1 +1,14 @@
 ﻿namespace StringUtils;
+
+public static class StringUtilities
+{
+    public static string ToUpper(string s)
+    {
+        foreach (char letter in s)
+        {
+            
+        }
+        return s;/*temp error fix to save my annoyance*/
+    }
+
+}
