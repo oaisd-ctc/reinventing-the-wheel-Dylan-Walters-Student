80acae7151268cdf59cf7e482c7454cfeb2821a2
Alexander-Wilhelm-Student <112420921+Alexander-Wilhelm-Student@users.noreply.github.com>
2023-01-24 18:30:39 +0000

    mode, plus EVEN MORE METHODS WHYYYYYYYYY

--- ArrayUtils.cs
+++ ArrayUtils.cs
@@ -559,12 +559,102 @@
             else throw new Exception();
         }
 
-        public int Mode(int[] nums) {  // AFAIK i'm not allowed to use dictionaries, given that they're defined in System.Collections.Generic. That's not gonna stop me.
-            int[] values = new int[0];
-            int[] counts = new int[0];
+        public int Mode(int[] nums)  // AFAIK i'm not allowed to use dictionaries, given that they're defined in System.Collections.Generic. That's not gonna stop me.
+        {
+            int[] values = new int[0]; //list of unique values in the array
+            int[] counts = new int[0]; //corresponding array of their number of occurrences
 
+            foreach (int i in nums) //get entries of all of the numbers and their number of occurrences
+            {
+                if (!Contains(values, i)) //if values doesn't have an entry for a number
+                {
+                    Append(values, i); //create one, and a corresponding entry in counts containing its occurrences
+                    Append(counts, HowMany(nums, i));
+                }
+            }
+
+            int maxcount = Max(counts); //get the number of occurrences of the mode
+
+            if (HowMany(counts, maxcount) > 1) //if there is more than one number with that number of occurrences (no mode!)
+            { 
+                return -1; // return -1 to indicate such
+            }
+
+            //otherwise, we know there's a mode!
+            return values[IndexOf(counts, maxcount)]; //return the corresponding value from the values array
         }
 
+        public float Mode(float[] nums) 
+        {
+            float[] values = new float[0];
+            int[] counts = new int[0];
+
+            foreach (int i in nums)
+            {
+                if (!Contains(values, i))
+                {
+                    Append(values, i);
+                    Append(counts, HowMany(nums, i));
+                }
+            }
+
+            int maxcount = Max(counts);
+
+            if (HowMany(counts, maxcount) > 1)
+            { 
+                return -1;
+            }
+
+            return values[IndexOf(counts, maxcount)];
+        }
+
+        public double Mode(double[] nums) 
+        {
+            double[] values = new double[0];
+            int[] counts = new int[0];
+
+            foreach (int i in nums)
+            {
+                if (!Contains(values, i))
+                {
+                    Append(values, i);
+                    Append(counts, HowMany(nums, i));
+                }
+            }
+
+            int maxcount = Max(counts);
+
+            if (HowMany(counts, maxcount) > 1)
+            { 
+                return -1;
+            }
+
+            return values[IndexOf(counts, maxcount)];
+        }
+
+        public decimal Mode(decimal[] nums) 
+        {
+            decimal[] values = new decimal[0];
+            int[] counts = new int[0];
+
+            foreach (int i in nums)
+            {
+                if (!Contains(values, i))
+                {
+                    Append(values, i);
+                    Append(counts, HowMany(nums, i));
+                }
+            }
+
+            int maxcount = Max(counts);
+
+            if (HowMany(counts, maxcount) > 1)
+            { 
+                return -1;
+            }
+
+            return values[IndexOf(counts, maxcount)];
+        }
 
         //append
         public int[] Append(int[] x, int y)
@@ -601,9 +691,12 @@
 
 
         //contains
-        public bool Contains(int[]x, int y) {
-            foreach (int i in x) {
-                if (i == y) {
+        public bool Contains(int[] x, int y)
+        {
+            foreach (int i in x)
+            {
+                if (i == y)
+                {
                     return true; //return early to terminate the function
                 }
             }
@@ -611,9 +704,12 @@
             return false;
         }
 
-        public bool Contains(float[]x, float y) {
-            foreach (float i in x) {
-                if (i == y) {
+        public bool Contains(float[] x, float y)
+        {
+            foreach (float i in x)
+            {
+                if (i == y)
+                {
                     return true; //return early to terminate the function
                 }
             }
@@ -621,9 +717,12 @@
             return false;
         }
 
-        public bool Contains(double[]x, double y) {
-            foreach (double i in x) {
-                if (i == y) {
+        public bool Contains(double[] x, double y)
+        {
+            foreach (double i in x)
+            {
+                if (i == y)
+                {
                     return true; //return early to terminate the function
                 }
             }
@@ -631,14 +730,142 @@
             return false;
         }
 
-        public bool Contains(decimal[]x, decimal y) {
-            foreach (decimal i in x) {
-                if (i == y) {
+        public bool Contains(decimal[] x, decimal y)
+        {
+            foreach (decimal i in x)
+            {
+                if (i == y)
+                {
                     return true; //return early to terminate the function
                 }
             }
 
             return false;
+        }
+
+
+        // indexof
+        public int IndexOf(int[] x, int y)
+        {
+            if (Contains(x, y))
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (x[i] == y)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+            else return -1;
+        }
+
+        public int IndexOf(float[] x, float y)
+        {
+            if (Contains(x, y))
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (x[i] == y)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+            else return -1;
+        }
+
+        public int IndexOf(double[] x, double y)
+        {
+            if (Contains(x, y))
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (x[i] == y)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+            else return -1;
+        }
+
+        public int IndexOf(decimal[] x, decimal y)
+        {
+            if (Contains(x, y))
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (x[i] == y)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+            else return -1;
+        }
+
+
+
+        // howmany
+        public int HowMany(int[] x, int y)
+        {
+            int count = 0;
+
+            foreach (int i in x)
+            {
+                if (i == y)
+                {
+                    count++;
+                };
+            }
+            return count;
+        }
+
+        public int HowMany(float[] x, float y)
+        {
+            int count = 0;
+
+            foreach (float i in x)
+            {
+                if (i == y)
+                {
+                    count++;
+                };
+            }
+            return count;
+        }
+
+        public int HowMany(double[] x, double y)
+        {
+            int count = 0;
+
+            foreach (double i in x)
+            {
+                if (i == y)
+                {
+                    count++;
+                };
+            }
+            return count;
+        }
+
+        public int HowMany(decimal[] x, decimal y)
+        {
+            int count = 0;
+
+            foreach (decimal i in x)
+            {
+                if (i == y)
+                {
+                    count++;
+                };
+            }
+            return count;
         }
 
     }
